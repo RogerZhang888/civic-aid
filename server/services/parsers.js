16500fd770eb9eb5de88de023bc6fb3648051865
palpatinezw <emperorpalpatinezw@gmail.com>
2025-04-19 16:53:55 +0800

    Report parser

--- server/services/parsers.js
+++ server/services/parsers.js
@@ -11,7 +11,13 @@ const extractJson = (raw) => {
     const jsonRegex = /\{\s*(?:"[^"]+"\s*:\s*[^,{}]+)(\s*,\s*"[^"]+"\s*:\s*[^,{}]+)*\s*\}/
     const sourceRegex = /[sS]ource[\s][0-9]+/
     let jsonSection = raw.match(jsonRegex)
-    let obj = JSON.parse(jsonSection)
+    let obj = {}
+    try {
+        obj = JSON.parse(jsonSection)
+    } catch {
+        console.log("Invalid JSON parse", jsonSection)
+        return {invalid:true}
+    }
     let parsedobj = {}
     for (let key in obj) {
         if (key.match(sourceRegex)) {
@@ -30,6 +36,7 @@ export const responseParsers = {
         // QUESTION, REPORT + CONFIDENCE, VALID
         let r = {type: undefined, valid: undefined, confidence: undefined}
         let parsed = extractJson(res)
+        if (parsed.invalid) return {valid:false}
 
         if (!("type" in parsed) || !("confidence" in parsed)) r.valid = false
         else {
@@ -45,7 +52,8 @@ export const responseParsers = {
     defaultParser: (res) => {
         // Just standard answer + confidence parser
         let r = {answer: undefined, valid: undefined, confidence: undefined, sources: undefined}
-        let parsed = JSON.parse(res)
+        let parsed = extractJson(res)
+        if (parsed.invalid) return {valid:false}
 
         if (!("answer" in parsed) || !("confidence" in parsed) || !("sources" in parsed)) r.valid = false
         else {
@@ -60,6 +68,22 @@ export const responseParsers = {
         return r
     },
     reportParser: (res) => {
-        return JSON.parse(res)
+        // For reports
+        let r = {summary: undefined, agency: undefined, confidence: undefined, sources: undefined, urgency:undefined, recommendedSteps:undefined}
+        let parsed = extractJson(res)
+        if (parsed.invalid) return {valid:false}
+
+        if (
+            !("summary" in parsed) ||
+            !("agency" in parsed) ||
+            !("confidence" in parsed) ||
+            !("sources" in parsed) ||
+            !("urgency" in parsed) ||
+            !("recommendedSteps" in parsed)
+        ) r.valid = false
+        else {
+            r = parsed
+            r.valid = true
+        }
     }
 }
