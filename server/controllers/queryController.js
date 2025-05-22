798ec97718ad0094c8a8d9f6e09399a74d554c57
palpatinezw <emperorpalpatinezw@gmail.com>
2025-05-22 15:44:18 +0800

    Bug fix

--- server/controllers/queryController.js
+++ server/controllers/queryController.js
@@ -126,7 +126,7 @@ const userquery = async (userprompt, userId, chatId, chat, location, media) => {
     if (userprompt == "") {
         if (media == "") throw new Error("Invalid prompt")
         // MEDIA ONLY
-        userprompt = await queryLLM({query:"", prompt:"", model:"captioner"}, responseParsers.noParser, reply='ALWAYS').then((res) => {
+        userprompt = await queryLLM({query:"", prompt:"", model:"captioner"}, responseParsers.noParser, 'ALWAYS').then((res) => {
             return res.answer
         })
     }
@@ -204,10 +204,10 @@ const submitQuery = async (req, res) => {
         const userId = req.user?.id || null;
         const uploadedFile = req.file;
 
-        console.log("Received prompt:", prompt);
-        console.log("Location:", latitude, longitude);
-        console.log("User ID:", userId);
-        console.log("Uploaded file:", uploadedFile);
+        // console.log("Received prompt:", prompt);
+        // console.log("Location:", latitude, longitude);
+        // console.log("User ID:", userId);
+        // console.log("Uploaded file:", uploadedFile);
 
         if ((!prompt) && (!uploadedFile)) {
             return res.status(400).json({ error: "Prompt is required" });
